9ec4730e819278341515a7b376da880f9af41236
squid233 <513508220@qq.com>
2021-03-09 20:40:12 +0800

    texture

---
+++ chapter2/texture-src/src/main/java/io/github/overrun/ldgt/client/DummyGame.java
@@ -0,0 +1,62 @@
+package io.github.overrun.ldgt.client;
+
+import io.github.overrun.ldgt.block.Block;
+
+import java.io.Closeable;
+
+import static org.lwjgl.glfw.GLFW.*;
+
+/**
+ * @author squid233
+ * @since 2021/03/04
+ */
+public final class DummyGame implements IGameLogic, Closeable {
+    private final GameRenderer renderer = new GameRenderer();
+
+    @Override
+    public void init() {
+        renderer.init();
+    }
+
+    @Override
+    public void input(Window window) {
+        Block block = renderer.block;
+        if (window.isKeyPressed(GLFW_KEY_W)) {
+            ++block.y;
+        }
+        if (window.isKeyPressed(GLFW_KEY_S)) {
+            --block.y;
+        }
+        if (window.isKeyPressed(GLFW_KEY_A)) {
+            ++block.x;
+        }
+        if (window.isKeyPressed(GLFW_KEY_D)){
+            --block.x;
+        }
+        if (window.isKeyPressed(GLFW_KEY_UP)) {
+            block.scale += .1f;
+        }
+        if (window.isKeyPressed(GLFW_KEY_DOWN)) {
+            block.scale -= .1f;
+        }
+        if (window.isKeyPressed(GLFW_KEY_LEFT)) {
+            block.rotation -= .1f;
+        }
+        if (window.isKeyPressed(GLFW_KEY_RIGHT)) {
+            block.rotation += .1f;
+        }
+    }
+
+    @Override
+    public void update(float delta) { }
+
+    @Override
+    public void render(Window window) {
+        renderer.render(window);
+    }
+
+    @Override
+    public void close() {
+        renderer.close();
+    }
+}
